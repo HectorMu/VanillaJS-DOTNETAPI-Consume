005bf3cc9d04b8eb68a404f143400043c79d3c5c
HectorMu <skyspartanblack@gmail.com>
2021-10-20 00:30:59 -0500

    xd

--- index.js
+++ index.js
@@ -24,13 +24,11 @@ const RenderAlumn = async(id) =>{
     if(alumno.length>0){
         alumno.forEach(dato => {
             alumndiv.innerHTML = `
-          
             <h2>Matricula: ${dato.matricula}</h2>
             <p>Nombre: ${dato.nombre}</p>
             <p>Carrera: ${dato.carrera}</p>
             <p>Semestre: ${dato.semestre}</p>
             <p>Saldo: ${dato.saldo}</p>
-       
             `
         });  
     }else{
@@ -48,19 +46,25 @@ const btnGuardar = document.getElementById('btnGuardar')
 
 btnGuardar.addEventListener('click',async(e)=>{
     e.preventDefault()
-    alertdiv.classList.remove('display-none')
-    alertdiv.innerText="Guardado"
-    await guardarAlumno(Nombre.value,Carrera.value,Semestre.value,Saldo.value)
+    const status = await guardarAlumno(Nombre.value,Carrera.value,Semestre.value,Saldo.value)
+    if(status.ok){
+        alertdiv.classList.remove('display-none')
+        alertdiv.classList.add('alert-success')
+        alertdiv.innerText="Guardado"
+    }else{
+        alertdiv.classList.remove('display-none')
+        alertdiv.classList.add('alert-error')
+        alertdiv.innerText="Error al guardar"
+    }
 })
 
 const guardarAlumno = async(nombre,carrera,semestre,saldo) =>{
     //development server source
     //let url = `http://localhost:84/Principal/AlmacenarSQLServer?Nombre=${nombre}&Carrera=${carrera}&Semestre=${semestre}&Saldo=${saldo}`
     //azure API REST with AZURE SQL server database
-    let url = `https://apirestsqlserver20211019194547.azurewebsites.net/AlmacenarSQLServer?Nombre=${nombre}&Carrera=${carrera}&Semestre=${semestre}&Saldo=${saldo}`
+    let url = `https://apirestsqlserver20211019194547.azurewebsites.net/Principal/AlmacenarSQLServer?Nombre=${nombre}&Carrera=${carrera}&Semestre=${semestre}&Saldo=${saldo}`;
     const response = await fetch(url)
-    const result = await response.json()
-    console.log(result)
+    const result = await response
     return result
 }
 
